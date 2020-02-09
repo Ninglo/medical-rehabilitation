e08653cca0ea6a1f9d9fe09df78091081469687d
Ninglo <1871391898@qq.com>
2020-02-09 10:19:53 +0800

    .

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,27 +1,18 @@
 //app.js
-App(
-  {
-    onLaunch: function ()
-    {
-      if (!wx.cloud)
-      {
-        console.error('请使用 2.2.3 或以上的基础库以使用云能力')
-      }
-      else
-      {
-        wx.cloud.init(
-          {
-            env: 'dev-id-ylkf',
-            traceUser: true,
-          }
-        )
-      }
-      this.globalData =
-      {
-        header: { 'Cookie': '' },
-        userData: false,
-        className: '请选择您的课程',
-      }
+App({
+  onLaunch: function() {
+    if (!wx.cloud) {
+      console.error("请使用 2.2.3 或以上的基础库以使用云能力");
+    } else {
+      wx.cloud.init({
+        env: "dev-id-ylkf",
+        traceUser: true
+      });
     }
+    this.globalData = {
+      header: { Cookie: "" },
+      userData: false,
+      className: "请选择您的课程"
+    };
   }
-)
+});
