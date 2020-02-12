a2171a1feab4b69ccf1d59bdfb0e2aaed3464421
czy2000 <60008125+czy2000@users.noreply.github.com>
2020-02-12 23:08:03 +0800

    课程选择页 (#7)
    * Add files via upload
    
    * Delete 课程选择页.zip
    
    * Delete course.js
    
    * Delete course.json
    
    * Delete course.wxml
    
    * Delete course.wxss
    
    * Add files via upload

--- miniprogram/pages/course/course.js
+++ miniprogram/pages/course/course.js
@@ -1,12 +1,51 @@
 // miniprogram/pages/course/course.js
+const app = getApp()
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    courses: []
+    courses: [],
+    oneSentence:'查看更多',
+    listLen:1,
+    navbartxt: ['课程类型1','2','3','4'],
+    course:[
+      {
+        courseTitle:'康复课程xxx',
+        courseIntdc:'一段介绍 | 时间',
+        url:'./detail/detail.wxml'
+      },
+      {
+        courseTitle: '康复课程xxx',
+        courseIntdc: '一段介绍 | 时间',
+        url:''
+      },
+      {
+        courseTitle: '康复课程xxx',
+        courseIntdc: '一段介绍 | 时间',
+        url:''
+      },
+      {
+        courseTitle: '康复课程xxx',
+        courseIntdc: '一段介绍 | 时间',
+        url:''
+      },
+      {
+        courseTitle: '康复课程xxx',
+        courseIntdc: '一段介绍 | 时间',
+        url:''
+      }
+    ],
   },
-
+  viewMore: function () {
+    var listLen = this.data.listLen + 1;
+    console.log(listLen);
+    this.setData(
+      {
+        listLen: listLen
+      }
+    )
+  },
   /**
    * 生命周期函数--监听页面加载
    */
