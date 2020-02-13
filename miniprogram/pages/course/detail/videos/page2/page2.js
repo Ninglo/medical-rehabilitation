8663f0f3299108fc68b7e541bdea349801ae1f47
F-993 <59988920+F-993@users.noreply.github.com>
2020-02-13 19:36:45 +0800

    video (#9)
    * Delete video.wxss
    
    * Delete video.wxml
    
    * Delete video.json
    
    * Delete video.js
    
    * Add files via upload
    
    * Update app.json

---
+++ miniprogram/pages/course/detail/videos/page2/page2.js
@@ -0,0 +1,78 @@
+// pages/index/page2/page2.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    text1: "这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍",
+    text2: "这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍",
+    text3: "这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍这是一段介绍",
+    video1: "https://media.w3.org/2010/05/sintel/trailer.mp4",
+    video2: "http://www.w3school.com.cn/example/html5/mov_bbb.mp4",
+    video3: "https://www.w3schools.com/html/movie.mp4",
+    video4: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4",
+    video5: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8",
+
+  },
+  clickButton: function () {
+    wx.navigateTo({
+      url: '../page3/page3'
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
