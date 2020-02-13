b01337432ed2b760bb05770f280c8d12cb0a6be5
youcan-666 <60022753+youcan-666@users.noreply.github.com>
2020-02-13 14:26:30 +0800

    聊天界面简易版 (#10)
    * Update chatroom.js
    
    * Update chatroom.wxml
    
    * Update chatroom.wxss

--- miniprogram/pages/service/chatroom/chatroom.js
+++ miniprogram/pages/service/chatroom/chatroom.js
@@ -1,80 +1,139 @@
 // miniprogram/pages/service/chatroom/chatroom.js
+const app = getApp();
+var inputVal = '';
+var msgList = [];
+var windowWidth = wx.getSystemInfoSync().windowWidth;
+var windowHeight = wx.getSystemInfoSync().windowHeight;
+var keyHeight = 0;
+
+
+
+
+/**
+ * 初始化数据
+ */
+function initData(that) {
+  inputVal = '';
+
+  msgList = [{
+      speaker: 'server',
+      contentType: 'text',
+      content: 'Hello,world！'
+    }
+  ]
+  that.setData({
+    msgList,
+    inputVal
+  })
+}
+
+/**
+ * 计算msg总高度
+ */
+// function calScrollHeight(that, keyHeight) {
+//   var query = wx.createSelectorQuery();
+//   query.select('.scrollMsg').boundingClientRect(function(rect) {
+//   }).exec();
+// }
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
-  data:
-  {
-    doctor: ''
+  data: {
+    lists: [
+      //所有发送的信息
+    ],
+    scrollHeight: '100vh',
+    inputBottom: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options)
-  {
-    console.log(options)
-    this.setData(
-      {
-        doctor: options.doctor
-      }
-    )
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function ()
-  {
-
+  onLoad: function(options) {
+    initData(this);
+    this.setData({
+      cusHeadIcon: app.globalData.userInfo.avatarUrl,
+    });
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function ()
-  {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function ()
-  {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function ()
-  {
+  onShow: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function ()
-  {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function ()
-  {
+  onReachBottom: function() {
 
   },
 
   /**
-   * 用户点击右上角分享
+   * 获取聚焦
    */
-  onShareAppMessage: function ()
-  {
+  focus: function(e) {
+    keyHeight = e.detail.height;
+    this.setData({
+      scrollHeight: (windowHeight - keyHeight) + 'px'
+    });
+    this.setData({
+      toView: 'msg-' + (msgList.length - 1),
+      inputBottom: keyHeight + 'px'
+    })
+    //计算msg高度
+    // calScrollHeight(this, keyHeight);
 
+  },
+  
+
+  //失去聚焦(软键盘消失)
+  blur: function(e) {
+    this.setData({
+      scrollHeight: '100vh',
+      inputBottom: 发送
+    })
+    this.setData({
+      toView: 'msg-' + (msgList.length - 1)
+    })
+
+  },
+
+  /**
+   * 发送点击监听
+   */
+  sendClick: function(e) {
+    msgList.push({
+      speaker: 'customer',
+      contentType: 'text',
+      content: e.detail.value
+    })
+    inputVal = '';
+    this.setData({
+      msgList,
+      inputVal
+    });
+
+
+  },
+
+  /**
+   * 退回上一页
+   */
+  toBackClick:function() {
+    wx.navigateBack({})
   }
+
 })
