b2be1a70c7dd4119f0153b5ec454d7b7d3cacfd1
Ninglo <48613687+Ninglo@users.noreply.github.com>
2020-03-10 15:47:06 +0800

    fix

---
+++ miniprogram/pages/course/blank/markd.md
@@ -0,0 +1,5 @@
+# It is show time
+
+$$
+\overline{AB}+\overline{BC}+\overline{CA}
+$$
--- miniprogram/pages/service/consultation/consultation.js
+++ miniprogram/pages/service/consultation/consultation.js
@@ -1,108 +1,96 @@
 // pages/consultation/consultation.js
 Page({
-
   /**
    * 页面的初始数据
    */
-  data:
-  {
-      select: false,
-      diseaseType: '咨询种类',
-      docterType: [{
-          text: '专家',
-          url: '../chooseDoctor/chooseDoctor'
-      }, {
-          text: '专业医生',
-          url: '../chooseDoctor/chooseDoctor'
-      }, {
-          text: '营养咨询师',
-          url: '../chooseDoctor/chooseDoctor'
-      }, {
-          text: 'XX医生',
-          url: '../chooseDoctor/chooseDoctor'
-      }]
+  data: {
+    select: false,
+    diseaseType: "咨询种类",
+    illNames: ["神经系统", "骨关节疾病", "心血管疾病", "老年康复"],
+    docterType: [
+      {
+        text: "专家",
+        url: "../chooseDoctor/chooseDoctor"
+      },
+      {
+        text: "专业医生",
+        url: "../chooseDoctor/chooseDoctor"
+      },
+      {
+        text: "营养咨询师",
+        url: "../chooseDoctor/chooseDoctor"
+      },
+      {
+        text: "XX医生",
+        url: "../chooseDoctor/chooseDoctor"
+      }
+    ]
   },
   bindShowMsg() {
     this.setData({
       select: !this.data.select
-    })
+    });
   },
   mySelect(e) {
-    var name = e.currentTarget.dataset.name
+    var name = e.currentTarget.dataset.name;
     this.setData({
       diseaseType: name,
       select: false
-    })
+    });
   },
-  naviGateToChooseDoctor: function (e) {
-    let url = e.currentTarget.dataset.url
+  naviGateToChooseDoctor: function(e) {
+    let url = e.currentTarget.dataset.url;
     if (url) {
       wx: wx.navigateTo({
         url: url,
-        success: function (res) { },
-        fail: function (res) { },
-        complete: function (res) { },
-      })
+        success: function(res) {},
+        fail: function(res) {},
+        complete: function(res) {}
+      });
     } else {
       wx.showModal({
-        title: '提示',
-        content: '功能还在开发中...',
-      })
+        title: "提示",
+        content: "功能还在开发中..."
+      });
     }
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function() {}
+});
